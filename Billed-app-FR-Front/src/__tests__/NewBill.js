bf06e8c28e064981ae265229159bf07b56593054
jucroizer <81417866+jucroizer@users.noreply.github.com>
2022-11-09 17:50:53 +0100

    premiers tests implementes sur la page NewBills

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -2,17 +2,59 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
+//import { screen } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
+import {localStorageMock} from "../__mocks__/localStorage.js";
+import router from "../app/Router.js";
+import { ROUTES, ROUTES_PATH } from "../constants/routes.js"
+import {screen, waitFor} from "@testing-library/dom"
+import { fireEvent } from "@testing-library/dom";
 
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
+    test("Then bill icon in vertical layout should be highlighted", async () =>  {
       const html = NewBillUI()
       document.body.innerHTML = html
       //to-do write assertion
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.append(root)
+      router()
+      window.onNavigate(ROUTES_PATH.NewBill)
+      await waitFor(() => screen.getByTestId('icon-window'))
+      const windowIcon = screen.getByTestId('icon-window')
+      //to-do write expect expression
+      expect(windowIcon).toBeTruthy()
     })
+  })
+})
+
+describe("Given that I am an employee trying to make a New Bill", () => {
+  describe("When I do not fill fields and I click on Envoyer button", () => {
+    test("Then It should renders New Bill page", () => {
+      document.body.innerHTML = NewBillUI();
+
+      const inputDate = screen.getByTestId("datepicker");
+      expect(inputDate.value).toBe("");
+
+      const inputAmount = screen.getByTestId("amount");
+      expect(inputAmount.value).toBe("");
+
+      const inputFile = screen.getByTestId("file");
+      expect(inputFile.value).toBe("");
+
+      const form = screen.getByTestId("form-new-bill");
+      const handleSubmit = jest.fn((e) => e.preventDefault());
+
+      form.addEventListener("submit", handleSubmit);
+      fireEvent.submit(form);
+      expect(screen.getByTestId("form-new-bill")).toBeTruthy();
+    });
   })
 })
